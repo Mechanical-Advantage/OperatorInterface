dbedcba3b45bed2fa84971783ce76045cc4fc949
e-bonner <e-bonner@users.noreply.github.com>
2019-08-28 16:58:31 -0400

    Add support for analog inputs

--- Arduino/OperatorInterface/OperatorInterface.ino
+++ Arduino/OperatorInterface/OperatorInterface.ino
@@ -8,10 +8,14 @@
 #define BUTTON_COUNT 24
 #define FIRST_BUTTON_PIN 1
 #define DIRECT_BUTTON_COUNT 8
+#define ANALOG_INPUT_COUNT 6
 #define SERIAL_BAUD_RATE 9600
 
 Joystick_ Joystick = Joystick_(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_GAMEPAD, BUTTON_COUNT, 0, true, 
     true, true, true, true, true, false, false, false, false, false);
+typedef void (Joystick_::*joystickFunc)(int16_t);
+const joystickFunc axisFuncs[] = {&Joystick_::setXAxis, &Joystick_::setYAxis, &Joystick_::setZAxis, &Joystick_::setRxAxis, 
+    &Joystick_::setRyAxis, &Joystick_::setRzAxis};
 
 boolean eStopPressed;
 
@@ -31,6 +35,12 @@ void loop() {
     // Directly connected buttons (1-8)
     for (byte button = 0; button < DIRECT_BUTTON_COUNT; button++) {
         Joystick.setButton(button, !digitalRead(button + FIRST_BUTTON_PIN));
+    }
+
+    // Analog inputs
+    // The default axis range is 0-1023, which matches the ADC
+    for (byte input = 0; input < ANALOG_INPUT_COUNT; input++) {
+        (Joystick.*axisFuncs[input])(analogRead(input));
     }
     
     Joystick.sendState();
